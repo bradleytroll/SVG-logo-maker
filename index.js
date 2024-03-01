82fd1af029279161c9a49ed392b209ac0f41d019
Bradley Troll <bradleytroll@gmail.com>
2024-02-29 20:32:24 -0500

    Adds prompt questions.

--- index.js
+++ index.js
@@ -2,3 +2,33 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square} = require('./lib/shapes');
 
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Enter up to three characters for the logo",
+            name: "text",
+        },
+        {
+            type: "input",
+            message: "Enter a color for the text",
+            name: "textColor",
+        },
+        {
+            type: "list",
+            message: "Choose a shape for the logo",
+            name: "shape",
+            choices: [
+                'circle', 'triangle', 'square'
+            ],
+        },
+        {
+            type: "input",
+            message: "Enter a color to fill the shape",
+            name: "shapeColor",
+        },
+])
+    .then((answers) => {
+       //Now need to take answers and crate the shapes
+
+    })
